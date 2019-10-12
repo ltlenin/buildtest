ad83fd9e173a917fde2f89cd8d33b50dadfd889b
ltlenin <ltlenin@gmail.com>
2019-10-13 00:07:06 +0530

    asdfasdf

---
+++ precache-manifest.0927d21aa0f03e2609de3cae3b3ed662.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862112a80581148349863e132e82dc25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe31dd1f3c3609d5f3d",
+    "url": "/static/css/4.849a7a81.chunk.css"
+  },
+  {
+    "revision": "0d9d07bb55b813ed7591",
+    "url": "/static/css/main.0e2c4a15.chunk.css"
+  },
+  {
+    "revision": "8bda409c48849d6d4b1f",
+    "url": "/static/js/0.ae69eb33.chunk.js"
+  },
+  {
+    "revision": "660d429d00d42520763d",
+    "url": "/static/js/1.6c06ef62.chunk.js"
+  },
+  {
+    "revision": "0fe31dd1f3c3609d5f3d",
+    "url": "/static/js/4.b4fc91e4.chunk.js"
+  },
+  {
+    "revision": "0d9d07bb55b813ed7591",
+    "url": "/static/js/main.e0a1f22a.chunk.js"
+  },
+  {
+    "revision": "24a38f60935c3835bf0d",
+    "url": "/static/js/runtime~main.01d7e2ec.js"
+  },
+  {
+    "revision": "1290bd656d4b24d9092af4df3e9f4964",
+    "url": "/static/media/ines.1290bd65.png"
+  },
+  {
+    "revision": "f6bc4a7bcd29a2aa8b37c5c4c125395e",
+    "url": "/static/media/investQwiseLoginPage.f6bc4a7b.png"
+  },
+  {
+    "revision": "1f06dbe2c6a6cbefa481caaeead3be9e",
+    "url": "/static/media/loginbg.1f06dbe2.png"
+  },
+  {
+    "revision": "38e1ed594f5710a9615e4e6f000e0773",
+    "url": "/static/media/poppins-italic-webfont.38e1ed59.woff"
+  },
+  {
+    "revision": "809946d177acd6304cf4114ced91a81c",
+    "url": "/static/media/poppins-italic-webfont.809946d1.woff2"
+  },
+  {
+    "revision": "2f8fb31d89696fa5e6a4f6c80fb4b156",
+    "url": "/static/media/poppins-regular-webfont.2f8fb31d.woff"
+  },
+  {
+    "revision": "9a5d3011e57ed3a1cade41c7a4412e63",
+    "url": "/static/media/poppins-regular-webfont.9a5d3011.woff2"
+  },
+  {
+    "revision": "0a40dca17b315965ead83b4ec32d6e7f",
+    "url": "/static/media/userprofilebg.0a40dca1.jpg"
+  }
+]);
